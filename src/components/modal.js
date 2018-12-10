bd3a5d4359e12619f8d606cece870187b20e5d16
Leo Soai-Van <leo.soai-van@capterra.com>
2018-12-09 23:12:04 -0500

    Add Modal text

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import '../css/modal.css';
 
+const ModalHeader = styled.div`
+  display: flex;
+  font-size: 120px;
+  justify-content: center;
+  margin-top: 48px;
+  margin-bottom: 48px;
+`;
+
+const ModalBody = styled.div`
+  margin: auto;
+  text-align: center;
+`;
+
 const Modal = ({ handleClose, displayModal, gameResult, currentPlayer = {name: '', symbol: ''} }) => {
-  const displayClassName = (displayModal ? "modal display-block" : "modal display-none");
+  const displayClassName = (true ? "modal display-block" : "modal display-none");
 
   return (
     <div className={displayClassName}>
       <section className="modal-main">
-        {currentPlayer.name}
+        <ModalHeader>
+          Congratulations!
+        </ModalHeader>
+        <ModalBody>
+          With strategy, cunning, and ruthelessness, {currentPlayer.name} has claimed total victory.
+        </ModalBody>
         <button onClick={handleClose}>Close</button>
       </section>
     </div>
