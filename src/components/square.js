01570563c33e7e7239ba43f9f118ee8f6de3d5d5
Leo Soai-Van <leo.soai-van@capterra.com>
2018-12-12 23:23:29 -0500

    Fix linting issues

--- src/components/square.js
+++ src/components/square.js
@@ -1,43 +1,64 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Root = styled.div`
   box-sizing: border-box;
   width: 100%;
   height: 100%;
   background: white;
-`
+`;
 
 class Square extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       value: '',
-    }
+    };
   }
 
-  handleClick = () => {
-    let index = this.props.boardIndex
-    let currentPlayer = this.props.currentPlayer
-    let playerSymbol = currentPlayer.symbol
+  handleClick = (boardIndex, currentPlayer) => {
+    const { onPlayerClick } = this.props;
+    const { value } = this.state;
 
-    if (this.state.value === '') {
+    if (value === '') {
       this.setState(
-        {value: playerSymbol},
-        () => this.props.onPlayerClick(index, currentPlayer)
-      )
+        { value: currentPlayer.symbol },
+        () => onPlayerClick(boardIndex, currentPlayer),
+      );
     }
   }
 
 
   render() {
-    return(
-      <Root onClick={() => this.handleClick()}>
-        {this.state.value}
+    const {
+      boardIndex,
+      currentPlayer,
+    } = this.props;
+
+    const {
+      value,
+    } = this.state;
+
+    return (
+      <Root onClick={() => this.handleClick(boardIndex, currentPlayer)}>
+        {value}
       </Root>
-    )
+    );
   }
 }
+
+Square.propTypes = {
+  boardIndex: PropTypes.number,
+  onPlayerClick: PropTypes.func,
+  currentPlayer: PropTypes.shape,
+};
+
+Square.defaultProps = {
+  boardIndex: undefined,
+  onPlayerClick: () => {},
+  currentPlayer: {},
+};
 
 export default Square;
