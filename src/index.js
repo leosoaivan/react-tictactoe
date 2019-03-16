75dc33667cc94d438bd950f65d82e63b733cf043
LPSV <leo.soaivan@gmail.com>
2019-03-16 09:35:57 -0400

    Remove wrapper around App

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React, { } from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components';
 import Game from './components/game';
 
-const Root = styled.div`
-`;
-
 const App = () => (
-  <Root>
-    <Game />
-  </Root>
+  <Game />
 );
-
 
 ReactDOM.render(<App />, document.querySelector('#root'));
