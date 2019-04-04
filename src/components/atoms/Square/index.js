2139115ef4a72f6e30e00962e0c271331f7ba4c3
leosoaivan <leo.soaivan@gmail.com>
2019-04-03 22:00:04 -0400

    Replace text with images of symbols

--- src/components/atoms/Square/index.js
+++ src/components/atoms/Square/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import LetterX from '../../../images/Letter-X.png';
+import LetterO from '../../../images/Letter-O.png';
 
 const Root = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
   background: white;
+  height: 100%;
+  width: 100%;
 `;
 
-const Square = ({ onClick, value }) => (
-  <Root onClick={onClick}>
-    {value}
-  </Root>
-);
+const Symbol = styled.img`
+  box-sizing: border-box;
+  object-fit: contain;
+  max-height: 100%;
+  max-width: 100%;
+  padding: 5px;
+`;
+
+const Square = ({ onClick, value }) => {
+  const symbol = () => {
+    if (value === 'X') {
+      return <Symbol src={LetterX} alt="" />;
+    }
+
+    if (value === 'O') {
+      return <Symbol src={LetterO} alt="" />;
+    }
+
+    return null;
+  };
+
+  return (
+    <Root onClick={onClick}>
+      {symbol()}
+    </Root>
+  );
+};
 
 Square.propTypes = {
   onClick: PropTypes.func,
